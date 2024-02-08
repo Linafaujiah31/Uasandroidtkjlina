a7cd3cb7999119ba942f160530a6244c3d0a4c4d
Linafaujiah31 <linafaujiah222@gmail.com>
2024-02-08 08:42:58 +0700

    first commit

---
+++ app/src/main/java/com/example/uasandroidtkjlina/NoteRoomActivity.kt
@@ -0,0 +1,85 @@
+package com.example.uasandroidtkjlina
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.uasandroidtkjlina.db.NoteRoomDatabase
+import com.example.uasandroidtkjlina.model.Note
+import com.example.uasandroidtkjlina.databinding.ActivityNoteRoomBinding
+
+class NoteRoomActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNoteRoomBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityNoteRoomBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        val isDataInserted = sharedPreferences.getBoolean("isDataInserted", false)
+
+        if (!isDataInserted) {
+            addDummyData()
+            sharedPreferences.edit().putBoolean("isDataInserted", true).apply()
+        }
+
+        getNotesDate()
+
+        binding.floatingAdd.setOnClickListener {
+            startActivity(Intent(this, EditActivity::class.java))
+        }
+    }
+
+    private fun addDummyData() {
+        val database = NoteRoomDatabase.getDatabase(application)
+        val dao = database.getNoteDao()
+
+        val dummyDataList = listOf(
+            Note(
+                title = "2169700036",
+                body = "Lina Faujiah",
+                nilai = "100",
+                keterangan = "Lulus",
+                jumlahsks = "11",
+                hargasks = "15000"
+            )
+        )
+
+        for (dummyNote in dummyDataList) {
+            dao.insert(dummyNote)
+        }
+    }
+
+
+    private fun getNotesDate() {
+        val database = NoteRoomDatabase.getDatabase(application)
+        val dao = database.getNoteDao()
+        val listItems = arrayListOf<Note>()
+        listItems.addAll(dao.getAll())
+        setupRecyclerView(listItems)
+    }
+
+    private fun setupRecyclerView(listItems: ArrayList<Note>) {
+        binding.recycleViewMain.apply {
+            adapter = NoteAdapter(listItems, object : NoteAdapter.NoteListener {
+                override fun OnItemClicked(note: Note) {
+                    val intent = Intent(this@NoteRoomActivity, EditActivity::class.java)
+                    intent.putExtra(EditActivity().EDIT_NOTE_EXTRA, note)
+                    startActivity(intent)
+                }
+            })
+
+            layoutManager = LinearLayoutManager(this@NoteRoomActivity)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        getNotesDate()
+    }
+}
